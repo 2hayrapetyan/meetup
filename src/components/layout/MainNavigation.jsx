a41df1d67dafb21a122d78afdd011648293a79d3
Hayrapetyan <113103029+2hayrapetyan@users.noreply.github.com>
2023-08-05 12:32:43 +0400

    add burger menu

--- src/components/layout/MainNavigation.jsx
+++ src/components/layout/MainNavigation.jsx
@@ -1,7 +1,9 @@
 import classes from "./MainNavigation.module.css";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
+import BurgerMenu from "./BurgerMenu";
+import MenuItem from "@mui/material/MenuItem";
 
 function MainNavigation() {
   const { locale, locales, push, pathname, query, asPath } = useRouter();
@@ -33,36 +35,84 @@ function MainNavigation() {
     }),
     []
   );
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 768);
+    };
+    handleResize(); 
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
 
   return (
     <header className={classes.header}>
-      <div className={classes.logo}>{headerTexts[locale]?.title || "Fallback Title"}</div>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/">{headerTexts[locale]?.nav1 || "Fallback Nav 1"}</Link>
-          </li>
-          <li>
-            <Link href="/new-meetup">{headerTexts[locale]?.nav2 || "Fallback Nav 2"}</Link>
-          </li>
-          <li>
-            <select
-              value={locale}
-              onChange={handleLocaleChange}
-              className={classes.select}
-            >
-              <option value="" disabled>
-                {headerTexts[locale]?.lang || "Fallback Lang"}
+      <div className={classes.logo}>
+        {headerTexts[locale]?.title || "Fallback Title"}
+      </div>
+      {isSmallScreen ?
+      ( <BurgerMenu>
+        <MenuItem>
+          <Link href='/'>{headerTexts[locale]?.nav1 || "Fallback Nav 1"}</Link>
+        </MenuItem>
+        <MenuItem>
+          <Link href='/new-meetup'>
+            {headerTexts[locale]?.nav2 || "Fallback Nav 2"}
+          </Link>
+        </MenuItem>
+        <MenuItem>
+          <select
+            value={locale}
+            onChange={handleLocaleChange}
+            className={classes.select}
+          >
+            <option value='' disabled>
+              {headerTexts[locale]?.lang || "Fallback Lang"}
+            </option>
+            {locales.map((loc) => (
+              <option key={loc} value={loc}>
+                {loc}
               </option>
-              {locales.map((loc) => (
-                <option key={loc} value={loc}>
-                  {loc}
-                </option>
-              ))}
-            </select>
-          </li>
-        </ul>
-      </nav>
+            ))}
+          </select>
+        </MenuItem>
+      </BurgerMenu>) : (
+  <nav>
+  <ul>
+    <li>
+      <Link href='/'>
+        {headerTexts[locale]?.nav1 || "Fallback Nav 1"}
+      </Link>
+    </li>
+    <li>
+      <Link href='/new-meetup'>
+        {headerTexts[locale]?.nav2 || "Fallback Nav 2"}
+      </Link>
+    </li>
+    <li>
+      <select
+        value={locale}
+        onChange={handleLocaleChange}
+        className={classes.select}
+      >
+        <option value='' disabled>
+          {headerTexts[locale]?.lang || "Fallback Lang"}
+        </option>
+        {locales.map((loc) => (
+          <option key={loc} value={loc}>
+            {loc}
+          </option>
+        ))}
+      </select>
+    </li>
+  </ul>
+</nav>
+      )}
+
     </header>
   );
 }
